5cd5d8597bbb5784bd88d303bf9379da12010d3a
Siegfried <siegfried.bozza@renuo.ch>
2023-05-21 14:05:42 +0200

    update rewards.spec with http request FE + BE tests

--- client/cypress/integration/rewards.spec.js
+++ client/cypress/integration/rewards.spec.js
@@ -2,16 +2,31 @@
 
 describe('Rewards', () => {
 
-    it('should render the rewards page --- mock', () => {
-        // register intercept on GET API request
-        cy.intercept({ method: 'GET', url: '**/api/rewards' },
-            { fixture: 'rewards.json' })
-        // visit the rewards page
-        cy.visit('/rewards')
-        // assert that the rewards page contains the mocked rewards
-        cy.get('ul')
-            .should('contain', '100000 points for drinking 8 cups of water for 7 straight days')
-            .and('contain', '850000 points for fasting for 5 days straight')
-            .and('contain','250000 points for exercising for 3 straight days')
+    describe('Mock requests - FE tests', () => {
+        it('should render the rewards page --- mock', () => {
+
+            cy.intercept({method: 'GET', url: 'http://localhost:4000/api/rewards'},
+                { fixture: 'rewards.json' })
+
+            cy.visit('/rewards')
+
+            // assert that the rewards page contains the mocked rewards
+            cy.get('ul')
+                .should('contain', '100000 points for drinking 8 cups of water for 7 straight days')
+                .and('contain', '850000 points for fasting for 5 days straight')
+                .and('contain', '250000 points for exercising for 3 straight days')
+        })
+    })
+
+    describe('API call - BE tests', () => {
+        it('should send a response status OK and body containing a rewards property --- API', () => {
+
+            cy.request('GET', 'http://localhost:4000/api/rewards')
+                .should((response) => {
+                    expect(response.status).to.eq(200)
+                    expect(response.body).to.have.property('rewards')
+                })
+        })
+
     })
 })
