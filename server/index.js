0ac4e08dc89f94f11ef9636af1185fc69047e0c1
Siegfried <siegfried.bozza@renuo.ch>
2023-05-24 18:53:23 +0200

    fix build path in server.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json())
 app.use(cors())
-app.use(express.static("./client/build"))
+app.use(express.static("./build"))
 
 app.post("/api/accomplishments", (req, res) => {
 
